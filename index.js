21043d0e1a252e623c7e556280a0bb9bec2a99ae
Ryan Lazzareschi <57603530+ryanlazz16@users.noreply.github.com>
2021-10-19 21:25:34 -0700

    Added Digital Dialects

--- index.js
+++ index.js
@@ -1,85 +1,121 @@
-const numProjects = 7;
+const numProjects = 8;
 
 gsap.registerPlugin(ScrollTrigger);
 
 // Hero Section
 let t1 = gsap.timeline({
-     scrollTrigger: {
-          trigger: '.hero',
-          // start: 'top 80%',
-          // end: 'bottom 20%',
-          toggleActions: 'restart reverse restart reverse',
-     }
+	scrollTrigger: {
+		trigger: '.hero',
+		// start: 'top 80%',
+		// end: 'bottom 20%',
+		toggleActions: 'restart reverse restart reverse',
+	},
 });
-t1.from('.about h4', {
-     duration: 1,
-     x: '100%',
-     opacity: 0,
-}, '0');
-t1.from('.about h1', {
-     duration: 1,
-     y: '-100%',
-     opacity: 0,
-}, '0');
-t1.from('.about h5', {
-     duration: 1,
-     x: '-100%',
-     opacity: 0,
-}, '0.5');
-t1.from('.about button', {
-     duration: 1,
-     y: '100%',
-     opacity: 0,
-     stagger: 0.1
-}, '1');
-t1.from('.profile', {
-     duration: 2,
-     y: '-200%',
-     opacity: 0,
-     ease: Bounce.easeOut,
-}, '0');
-t1.from('.profile', {
-     duration: 2,
-     x: '200%',
-     ease: Power1.easeOut,
-}, '0');
+t1.from(
+	'.about h4',
+	{
+		duration: 1,
+		x: '100%',
+		opacity: 0,
+	},
+	'0'
+);
+t1.from(
+	'.about h1',
+	{
+		duration: 1,
+		y: '-100%',
+		opacity: 0,
+	},
+	'0'
+);
+t1.from(
+	'.about h5',
+	{
+		duration: 1,
+		x: '-100%',
+		opacity: 0,
+	},
+	'0.5'
+);
+t1.from(
+	'.about button',
+	{
+		duration: 1,
+		y: '100%',
+		opacity: 0,
+		stagger: 0.1,
+	},
+	'1'
+);
+t1.from(
+	'.profile',
+	{
+		duration: 2,
+		y: '-200%',
+		opacity: 0,
+		ease: Bounce.easeOut,
+	},
+	'0'
+);
+t1.from(
+	'.profile',
+	{
+		duration: 2,
+		x: '200%',
+		ease: Power1.easeOut,
+	},
+	'0'
+);
 
 // Projects Title
 gsap.from('.projects h2', {
-     duration: 1,
-     x: '-100%',
-     opacity: 0,
-     scrollTrigger: {
-          trigger: '.project',
-          toggleActions: 'restart none none reverse'
-     },
+	duration: 1,
+	x: '-100%',
+	opacity: 0,
+	scrollTrigger: {
+		trigger: '.project',
+		toggleActions: 'restart none none reverse',
+	},
 });
 
 // Project Animations
-for (var i = 1; i<=numProjects; i++) {
-     var t2 = gsap.timeline({
-          scrollTrigger: {
-               trigger: '.p'+i.toString(),
-               // start: 'top 90%',
-               // end: 'bottom 10%',
-               toggleActions: 'restart none none reverse',
-          }
-     });
+for (var i = 1; i <= numProjects; i++) {
+	var t2 = gsap.timeline({
+		scrollTrigger: {
+			trigger: '.p' + i.toString(),
+			// start: 'top 90%',
+			// end: 'bottom 10%',
+			toggleActions: 'restart none none reverse',
+		},
+	});
 
-     t2.from('.p'+i.toString(), {
-          duration: 1,
-          opacity: 0,
-     }, '0');
+	t2.from(
+		'.p' + i.toString(),
+		{
+			duration: 1,
+			opacity: 0,
+		},
+		'0'
+	);
 
-     t2.from('.p'+i.toString()+' .project-left', {
-          duration: 1,
-          opacity: 0,
-          x: '-100%',
-     }, '0');
+	t2.from(
+		'.p' + i.toString() + ' .project-left',
+		{
+			duration: 1,
+			opacity: 0,
+			x: '-100%',
+		},
+		'0'
+	);
 
-     t2.from('.p'+i.toString()+' .project-right', {
-          duration: 1,
-          opacity: 0,
-          x: '100%',
-     }, '0');
+	t2.from(
+		'.p' + i.toString() + ' .project-right',
+		{
+			duration: 1,
+			opacity: 0,
+			x: '100%',
+		},
+		'0'
+	);
 }
